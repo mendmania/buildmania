bcd063a9d469f0d239d60306257b2d10bc516add
mendmania <mendim@gjirafa.com>
2023-11-26 22:37:17 +0100

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <SectionHero />
+    <SectionCards />
+    <SectionSquareInfo />
+    <SectionGrid />
+    <SectionGridInfo />
+    <SectionPricing />
+    <SectionQuote />
+    <SectionContact />
+  </div>
+</template>
+
+
+
+<style>
+</style>
